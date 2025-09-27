0fb9720e71633d810c673bba3e5a568c4c73041f
Jake Soenneker <jake@soenneker.com>
2025-09-27 09:04:36 -0500

    rename

---
+++ src/PointerEventsKeyword.cs
@@ -0,0 +1,15 @@
+using Intellenum;
+
+namespace Soenneker.Quark.Enums;
+
+/// <summary>
+/// Typed values for the CSS pointer-events property in .NET enumeration form.
+/// </summary>
+[Intellenum<string>]
+public sealed partial class PointerEventsKeyword
+{
+    public static readonly PointerEventsKeyword None = new("none");
+    public static readonly PointerEventsKeyword Auto = new("auto");
+
+    public static implicit operator PointerEventsKeyword(GlobalKeyword style) => new(style.Value);
+}
