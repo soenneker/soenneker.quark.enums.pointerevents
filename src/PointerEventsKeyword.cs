b2c0ec67a62525ae60f36e6cb51c8903c603dd3c
Jake Soenneker <jake@soenneker.com>
2026-02-27 08:25:26 -0600

    Deps

--- src/PointerEventsKeyword.cs
+++ src/PointerEventsKeyword.cs
@@ -6,10 +6,9 @@ namespace Soenneker.Quark.Enums;
 /// Typed values for the CSS pointer-events property in .NET enumeration form.
 /// </summary>
 [EnumValue<string>]
+[IncludeEnumValues(typeof(GlobalKeyword))]
 public sealed partial class PointerEventsKeyword
 {
     public static readonly PointerEventsKeyword None = new("none");
     public static readonly PointerEventsKeyword Auto = new("auto");
-
-    public static implicit operator PointerEventsKeyword(GlobalKeyword style) => new(style.Value);
 }
