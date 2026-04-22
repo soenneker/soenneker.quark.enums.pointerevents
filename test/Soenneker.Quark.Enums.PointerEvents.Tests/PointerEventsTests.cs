4e630b2648dbcce87b667a0cc2fe6cbea9f510a3
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:29 -0500

    TUnit conversion

--- test/Soenneker.Quark.Enums.PointerEvents.Tests/PointerEventsTests.cs
+++ test/Soenneker.Quark.Enums.PointerEvents.Tests/PointerEventsTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Quark.Enums.PointerEvents.Tests;
 
 public sealed class PointerEventsTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
